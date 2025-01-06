a65abe90b5e8a91c2ff7b3cb24e7d158bae3f47b
7chyngyz <sultanmuratovchyngyz@gmail.com7>
2025-01-06 16:13:45 +0600

    realy ready

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     images: {
       unoptimized: true,
     },
@@ -13,17 +12,3 @@ const nextConfig = {
   };
   
   export default nextConfig;
-=======
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
->>>>>>> d9084cab1e069026e88dbce95ce675b400e1e674
